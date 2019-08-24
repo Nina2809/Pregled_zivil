3bda8266feb282620352c32965a2393f08692142
Nina2809 <velkavrh.nina1234@gmail.com>
2019-08-24 17:47:37 +0200

    spremenila sem model, ker sem dodala datoteko json

--- model.py
+++ model.py
@@ -1,34 +1,45 @@
+import json
 import datetime
 
 
 class Zivilo:
     def __init__(self, tabela):
-        self.ime = tabela[0]
-        self.kategorija = tabela[1]
-        self.mascobe = tabela[2]
-        self.nasicene_mascobe = tabela[3]
-        self.sladkorji = tabela[4]
-        self.sol = tabela[5]
-        self.url = tabela[6]
+        self.ime = tabela["ime"]
+        self.kategorija = tabela["kategorija"]
+        self.mascobe = tabela["mascobe"]
+        self.nasicene_mascobe = tabela["nasicene_mascobe"]
+        self.sladkorji = tabela["sladkorji"]
+        self.sol = tabela["sol"]
+        self.url = tabela["url"]
 
 
-
-tabela_zivil = [["paradiznik", "zelenjava", 50, 20, 10, 10, "https://254066-790876-3-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2014/06/paradiznik.jpg"],
-                ["coca-cola", "gazirane pijace", 12, 13, 14, 15, "https://www.cokesolutions.com/content/cokesolutions/site/us/en/products/brands/coca-cola/coca-cola.main-image.434-624.png"]]
-
-
-
+'''
 def objekti():
+    metoda odpre datoteko zivila.json,
+    iz datoteke prebere zivila,
+    za vsakega vstvari razred Zivilo
+    in vrne list vseh razredov zivil
+'''
+def objekti():
+    with open('zivila.json') as f:
+        zivila = json.load(f)
     tabela_objektov = []
-    for i in range(len(tabela_zivil)):
-        tabela_objektov.append(Zivilo(tabela_zivil[i]))
-    
-    return(tabela_objektov)
 
-def dodaj_novo(tabela_novega):
-    tabela_zivil.append(tabela_novega)
-    
+    for obj in zivila:
+        tabela_objektov.append(Zivilo(obj))
 
+    return tabela_objektov
 
+'''
+def dodaj_novo():
+    odpremo datoteko zivila.json,
+    nalozimo v python, dodamo zivilo,
+    zapisemo vsa zivila nazaj v datoteko
+'''
+def dodaj_novo(slovar):
+    with open("zivila.json") as datoteka:
+        temp = json.load(datoteka)
 
-
+    temp.append(slovar)
+    with open("zivila.json", mode='w') as f:
+        f.write(json.dumps(temp, indent=2))
