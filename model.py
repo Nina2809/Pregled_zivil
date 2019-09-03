4f080f58cb5212fd666184c5a0c3d9df0c456469
Nina2809 <velkavrh.nina1234@gmail.com>
2019-09-03 14:44:16 +0200

    gumpki za kategorije, barve glede na ustreznost

--- model.py
+++ model.py
@@ -11,7 +11,16 @@ class Zivilo:
         self.sladkorji = tabela["sladkorji"]
         self.sol = tabela["sol"]
         self.url = tabela["url"]
-
+        if (self.kategorija == "sadni sokovi" or self.kategorija == "gazirane pijace"):
+            self.barva_mascobe = pijaca_profil(self.mascobe, "mascobe")
+            self.barva_nasicene = pijaca_profil(self.nasicene_mascobe, "nasicene_mascobe")
+            self.barva_sladkor = pijaca_profil(self.sladkorji, "sladkor")
+            self.barva_sol = pijaca_profil(self.sol, "sol")
+        else:
+            self.barva_mascobe = hrana_profil(self.mascobe, "mascobe")
+            self.barva_nasicene = hrana_profil(self.nasicene_mascobe, "nasicene_mascobe")
+            self.barva_sladkor = hrana_profil(self.sladkorji, "sladkor")
+            self.barva_sol = hrana_profil(self.sol, "sol")
 
 '''
 def objekti():
@@ -38,8 +47,32 @@ def dodaj_novo():
 '''
 def dodaj_novo(slovar):
     with open("zivila.json") as datoteka:
-        temp = json.load(datoteka)
+        zacasno = json.load(datoteka)
 
-    temp.append(slovar)
+    zacasno.append(slovar)
     with open("zivila.json", mode='w', encoding='utf-8') as f:
-        f.write(json.dumps(temp, indent=2))
+        f.write(json.dumps(zacasno, indent=2))
+
+
+# PREHRANSKI PROFIL
+
+pijaca = {'mascobe':[1.5, 10], 'nasicene_mascobe':[0.75, 2.5], 'sladkor':[2.5, 6.3], 'sol':[0.3, 1.5]}
+
+hrana = {'mascobe':[3, 20], 'nasicene_mascobe':[1, 5], 'sladkor':[5, 15], 'sol':[0.3, 1.5]}
+
+def pijaca_profil(vrednost, hranilne_vrednosti):
+    if (vrednost <= pijaca[hranilne_vrednosti][0]):
+        return "list-group-item-success"
+    elif (vrednost <= pijaca[hranilne_vrednosti][1]):
+        return "list-group-item-warning"
+    else:
+        return "list-group-item-danger"
+
+def hrana_profil(vrednost, hranilne_vrednosti):
+    if (vrednost <= hrana[hranilne_vrednosti][0]):
+        return "list-group-item-success"
+    elif (vrednost <= hrana[hranilne_vrednosti][1]):
+        return "list-group-item-warning"
+    else:
+        return "list-group-item-danger"
+
--- spletni_vmesnik.py
+++ spletni_vmesnik.py
@@ -5,6 +5,12 @@ from model import *
 
 tabela_zivil = []
 
+def pregled_metoda(kategorija_insert):
+    tabela_zivil = objekti()
+    kategorija = kategorija_insert
+    return bottle.template('zivilo_pogled', tabela_zivil_objektov=tabela_zivil, kategorija = kategorija)
+
+
 @bottle.get('/')
 
 def osnovna_stran():
@@ -14,17 +20,51 @@ def osnovna_stran():
 def naslednja_stran():
     return bottle.template('baza_zivil')
 
-
-@bottle.get('/zivilo_pogled')
-def pregled_zivil():
-    return bottle.template('zivilo_pogled')
-
-
-@bottle.get('/pregled')
+# POGLED GLEDE NA KATEGORIJO :
+@bottle.get('/pregled_vse')
 def pregled():
-    tabela_zivil = objekti()
-    kategorija = "vse"
-    return bottle.template('zivilo_pogled', tabela_zivil_objektov=tabela_zivil, kategorija = kategorija)
+    return pregled_metoda("vse")
+
+@bottle.get('/pregled_mlecni_izdelki')
+def pregled():
+    return pregled_metoda("mlecni izdelki")
+
+@bottle.get('/pregled_mesni_ribe')
+def pregled():
+    return pregled_metoda("mesni in ribji izdelki")
+
+@bottle.get('/pregled_riz_zito')
+def pregled():
+    return pregled_metoda("riz in zitni izdelki")
+
+@bottle.get('/pregled_pripravljene')
+def pregled():
+    return pregled_metoda("pripravljene jedi")
+
+@bottle.get('/pregled_zelenjavni')
+def pregled():
+    return pregled_metoda("zelenjavni izdelki")
+
+@bottle.get('/pregled_sadni')
+def pregled():
+    return pregled_metoda("sadni izdelki")
+
+@bottle.get('/pregled_sladkarije_slani')
+def pregled():
+    return pregled_metoda("sladkarije in slani prigrizki")
+
+@bottle.get('/pregled_gazirane')
+def pregled():
+    return pregled_metoda("gazirane pijace")
+
+@bottle.get('/pregled_sadni_sokovi')
+def pregled():
+    return pregled_metoda("sadni sokovi")
+
+#--------------------------------------
+
+
+
 
 @bottle.get('/dodajanje')
 def dodajanje():
@@ -34,7 +74,7 @@ def dodajanje():
 @bottle.get('/dodano')
 def dodajanje():
     slovar_novega = {
-        "ime": bottle.request.query.getunicode('ime'),
+        "ime": bottle.request.query['ime'],
         "kategorija": bottle.request.query['kategorija'],
         'mascobe': float(bottle.request.query['mascobe']),
         'nasicene_mascobe': float(bottle.request.query['nasiceneMascobe']),
@@ -49,6 +89,10 @@ def dodajanje():
 @bottle.get('/slike/<ime>')
 def slike(ime):
     return bottle.static_file(ime, root = 'slike')
+
+@bottle.get('/preveri_prehranski_profil')
+def preveri():
+    return bottle.template('preveri_prehranski_profil')
 
 
 
